a17755b1a3251182fe15f217677a387ec47b9bcd
cfvalente <caio99valente@hotmail.com>
2014-03-21 18:16:14 -0300

    Testando encode.

--- physusp/test/com/gedaeusp/ControllerTest.java
+++ physusp/test/com/gedaeusp/ControllerTest.java
@@ -1,5 +1,7 @@
 package com.gedaeusp;
 
+// TESTANDO LÕX’!!!√¿&
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -8,7 +10,7 @@ public class ControllerTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		fail("N„o implementado!");
 	}
 
 }
