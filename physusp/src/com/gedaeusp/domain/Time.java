5c270c7069d192ccd5a6e49495e3e0cb5a208f69
cfvalente <caio99valente@hotmail.com>
2014-04-02 16:01:30 -0300

    Calculo da energia gasta com metabolismo aerobio.

--- physusp/src/com/gedaeusp/domain/Time.java
+++ physusp/src/com/gedaeusp/domain/Time.java
@@ -4,14 +4,23 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
-public class TimeConverter {
+public class Time {
 
 	@SuppressWarnings("deprecation")
-	public static long convertDateToSeconds(String string) throws ParseException {
+	public static Integer convertDateToSeconds(String string) throws ParseException {
 	    DateFormat formatter = new SimpleDateFormat("hh:mm:ss");  
 	    Date date = (Date)formatter.parse(string);
 	    return date.getHours()*3600+date.getMinutes()*60+date.getSeconds();
+	}
+	
+	public static Integer timeRange(List<Integer> time) {
+		Integer timeRange = 0;
+		for (int i = 0; i < time.size() - 1; i++) {
+			timeRange += (time.get(i + 1) - time.get(i));
+		}
+		return timeRange;
 	}
 
 }
--- physusp/test/com/gedaeusp/domain/AerobicTest.java
+++ physusp/test/com/gedaeusp/domain/AerobicTest.java
@@ -2,39 +2,64 @@ package com.gedaeusp.domain;
 
 import static org.junit.Assert.assertEquals;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
+import br.com.caelum.vraptor.restfulie.headers.RestDefaults;
+
 import com.gedaeusp.domain.AerobicCalculator;
-import com.gedaeusp.domain.TimeConverter;
 
 public class AerobicTest {
 
 	private static double EPSILON = 0.000000000000000000000000001;
 
 	@Test
-	public void timeConverterTest() throws ParseException {
-		assertEquals(TimeConverter.convertDateToSeconds("02:01:24"), 7284);
-	}
-
-	@Test
-	public void aerobicCalculatorTest() {
-		assert (false);
-	}
-
-	@Test
-	public void calculateAerobicConsumptionTest() {
-		List<Double> Comsumption = new ArrayList<Double>();
-		List<Long> Time = new ArrayList<Long>();
-		Comsumption.add((double) 400);
-		Comsumption.add((double) 800);
-		Time.add((long) 10);
-		Time.add((long) 12);
-		assertEquals(AerobicCalculator.integrate(Comsumption, Time), 1200,
+	public void integrateTest() {
+		List<Double> comsumption = new ArrayList<Double>();
+		List<Integer> time = new ArrayList<Integer>();
+		comsumption.add((double) 400);
+		comsumption.add((double) 800);
+		time.add((int) 10);
+		time.add((int) 12);
+		assertEquals(AerobicCalculator.integrate(comsumption, time), 1200,
 				EPSILON);
 	}
 
+	@Test
+	public void calculateRestAerobicComsumption() {
+		List<Double> comsumption = new ArrayList<Double>();
+		List<Integer> time = new ArrayList<Integer>();
+		comsumption.add((double) 400);
+		comsumption.add((double) 200);
+		comsumption.add((double) 300);
+		comsumption.add((double) 500);
+		comsumption.add((double) 100);
+		time.add((int) 10);
+		time.add((int) 12);
+		time.add((int) 13);
+		time.add((int) 14);
+		time.add((int) 19);
+		assertEquals(AerobicCalculator.restComsumption(comsumption, time), 305.55555555555554,
+				EPSILON);
+	}
+	
+	@Test
+	public void AerobicComsumptionTest() {
+		List<Double> comsumption = new ArrayList<Double>();
+		List<Double> restComsumption = new ArrayList<Double>();
+		List<Integer> time = new ArrayList<Integer>();
+		List<Integer> restTime = new ArrayList<Integer>();
+		comsumption.add((double) 400);
+		comsumption.add((double) 800);
+		restComsumption.add((double) 200);
+		restComsumption.add((double) 400);
+		time.add((int) 10);
+		time.add((int) 14);
+		restTime.add((int) 1);
+		restTime.add((int) 10);
+		assertEquals(AerobicCalculator.calculateAerobicComsumption(comsumption,
+				restComsumption, time, restTime), 1200, EPSILON);
+	}
 }
