e10f2e6e3bd58080c680d24d8e9ecd207eb53720
Jorge Melegati <jorgeamg4@gmail.com>
2014-04-11 15:31:35 -0300

    Usando grids para entrada de dados de consumo de oxigênio. Reestruturando a página do gráfico.

--- physusp/WebContent/WEB-INF/jsp/charts/index.jsp
+++ physusp/WebContent/WEB-INF/jsp/charts/index.jsp
@@ -4,37 +4,64 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="<c:url value="/css/lib/jquery.handsontable.full.css"/>"/>
 		<title>PhysUSP</title>
 	</head>
 	<body>
 		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<form id="data" action="<c:url value="/charts/calculate"/>">
+			<form id="data" class="form-horizontal" action="<c:url value="/charts/calculate"/>">
+				<div class="row">
+					<div class="col-md-6">
+						<h1>Energy Systems Contributions</h1>
+						<p>Fill out the fields below with... </p>
 						<div class="form-group">
-							<label>Rest lactate concentration (mmol/L)</label> 
-							<input type="text" name="parameters.restLactateConcentration" class="form-control" />
-							<label>Maximum lactate concentration (mmol/L)</label>
-							<input type="text" name="parameters.maxLactateConcentration" class="form-control" />
-							<label>Subject mass (kg)</label>
-							<input type="text" name="parameters.weight" class="form-control" />
-							<label>Rest oxygen consumption file</label>
-							<input type="file" name="oxygenConsumptionRest" id="oxygenConsumptionRest"/>
-							<label>Exercise oxygen consumption file</label>
-							<input type="file" name="oxygenConsumption" id="oxygenConsumption" />
-							<button type="submit">Send</button>
+							<label class="col-md-7 control-label">Rest lactate concentration (mmol/L)</label> 
+							<div class="col-md-5">
+								<input type="text" name="parameters.restLactateConcentration" class="form-control" />
+							</div>
 						</div>
-					</form>
-				</div>
-				<div class="col-md-8">
-					<div id="results">
+						<div class="form-group">
+							<label class="col-md-7 control-label">Maximum lactate concentration (mmol/L)</label>
+							<div class="col-md-5">
+								<input type="text" name="parameters.maxLactateConcentration" class="form-control col-md-6" />
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-7 control-label">Subject mass (kg)</label>
+							<div class="col-md-5">
+								<input type="text" name="parameters.weight" class="form-control col-md-6" />
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div id="results"></div>
 					</div>
 				</div>
-			</div>
+				<div class="row">
+					<div class="col-md-4">
+						<label>Rest oxygen consumption</label>
+						<div id="oxygenConsumptionRest"></div>
+					</div>
+					<div class="col-md-4">
+						<label>Exercise oxygen consumption</label>
+						<div id="oxygenConsumption"></div>
+					</div>
+					<div class="col-md-4">
+						<label>Post-Exercise oxygen consumption</label>
+						<div id="oxygenConsumptionPost"></div>
+					</div>
+				</div>
+				<div class="row" style="margin-top: 15px">
+					<div class="col-md-12">
+						<button type="submit" class="btn btn-primary btn-lg pull-right">Create chart</button>
+					</div>
+				</div>
+			</form>
 		</div>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script type="text/javascript" src="<c:url value="/js/lib/highcharts.js"/>"></script>
 		<script type="text/javascript" src="<c:url value="/js/chart.js"/>"></script>
+		<script type="text/javascript" src="<c:url value="/js/lib/jquery.handsontable.full.js"/>"></script>
 	</body>
 </html>
--- physusp/WebContent/js/chart.js
+++ physusp/WebContent/js/chart.js
@@ -1,38 +1,55 @@
-var FilesLoader = function () {
-	this.files = [];
-	this.output = "";
-	
-	this.addFile = function(file, parameterName) {
-		this.files.push({file: file, loaded: false, parameterName: parameterName});
-	};
-	
-	this.loadFiles = function (callback) {
-		var $this = this;
-		this.files.forEach(function(item){
-			var fileReader = new FileReader();
-			fileReader.readAsText(item.file);
-			fileReader.onload = function(evt){
-				var object = {};
-				object[item.parameterName] = evt.target.result;
-				$this.output += "&" + $.param(object);
-				item.loaded = true;
-				if($this.hasLoadedAllFiles())
-					callback();
-			};
-		});
-	};
-	
-	this.hasLoadedAllFiles = function (){
-		var loadedAllFiles = true;
-		this.files.forEach(function(item){
-			if(!item.loaded) {
-				loadedAllFiles = false;
-			}
-		});
-		return loadedAllFiles;
-	};
-	
-};
+function prepareTableToSend(tableRows){
+	var toSend = "";
+	for(var i = 0; i < tableRows.length - 1; i++)
+		toSend += tableRows[i][0] + "," + tableRows[i][1] + "\n";
+	return toSend;
+}
+
+function sendFormData(url, formData) {
+	$.ajax({
+		url: url,
+		type: "POST",
+		data: formData
+	})
+	.done(function(result){
+		var data = []; 
+		$.each(result.consumption, function(key, value) { data.push([key, value]);});
+		showChart(data);
+	});
+}
+
+function showChart(data) {
+	$("#results").highcharts({
+		chart: {
+			plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+	    },
+	    title: {
+	        text: 'Energy system contribuitions'
+	    },
+	    tooltip: {
+	    	pointFormat: '{series.name}: <b>{point.y:.1f} kcal</b> <br/><b>{point.percentage:.1f}%</b>'
+	    },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} % ({point.y:.1f} kcal)'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Contribution',
+            data: data
+        }]
+	});
+}
 
 $(function(){
 	
@@ -41,62 +58,41 @@ $(function(){
 		var input = $(this).serialize();
 		var url = $(this).attr("action");
 		
-		var filesLoader = new FilesLoader();
+		var oxygenConsumptionRest = $('#oxygenConsumptionRest').handsontable('getData');
+		input += "&parameters.oxygenConsumptionRest=" + prepareTableToSend(oxygenConsumptionRest);
 		
-		filesLoader.addFile(document.getElementById("oxygenConsumptionRest").files[0], "parameters.oxygenConsumptionRest");
-		filesLoader.addFile(document.getElementById("oxygenConsumption").files[0], "parameters.oxygenConsumption");
+		var oxygenConsumption = $('#oxygenConsumption').handsontable('getData');
+		input += "&parameters.oxygenConsumption=" + prepareTableToSend(oxygenConsumption);
 		
-		filesLoader.loadFiles(function() {
-			sendDataForm(url, input + filesLoader.output);
-		});
+		sendFormData(url, input);
 		
 		return false;
 	});
 	
-	function sendDataForm(url, formData) {
-		$.ajax({
-			url: url,
-			type: "POST",
-			data: formData
-		})
-		.done(function(result){
-			var data = []; 
-			$.each(result.consumption, function(key, value) { data.push([key, value]);});
-			showChart(data);
-		});
-	}
+	var headers = ["Time (hh:mm:ss)", "Consumption (ml/min)"];
 	
-	function showChart(data)
-	{
-		$("#results").highcharts({
-			chart: {
-				plotBackgroundColor: null,
-	            plotBorderWidth: null,
-	            plotShadow: false
-		    },
-		    title: {
-		        text: 'Energy system contribuitions'
-		    },
-		    tooltip: {
-		    	pointFormat: '{series.name}: <b>{point.y:.1f} kcal</b> <br/><b>{point.percentage:.1f}%</b>'
-		    },
-	        plotOptions: {
-	            pie: {
-	                allowPointSelect: true,
-	                cursor: 'pointer',
-	                dataLabels: {
-	                    enabled: true,
-	                    color: '#000000',
-	                    connectorColor: '#000000',
-	                    format: '<b>{point.name}</b>: {point.percentage:.1f} % ({point.y:.1f} kcal)'
-	                }
-	            }
-	        },
-	        series: [{
-	            type: 'pie',
-	            name: 'Contribution',
-	            data: data
-	        }]
-		});
-	}
+	$('#oxygenConsumptionRest').handsontable({
+	    minSpareRows: 1,
+	    colHeaders: headers,
+	    contextMenu: true,
+	    data: [[null, null]],
+	    height: 300
+	});
+	
+	$('#oxygenConsumption').handsontable({
+	    minSpareRows: 1,
+	    colHeaders: headers,
+	    contextMenu: true,
+	    data: [[null, null]],
+	    height: 300
+	});
+	
+	$('#oxygenConsumptionPost').handsontable({
+	    minSpareRows: 1,
+	    colHeaders: headers,
+	    contextMenu: true,
+	    data: [[null, null]],
+	    height: 300
+	});
+	
 });
