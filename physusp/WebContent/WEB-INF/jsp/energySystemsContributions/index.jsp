ecad593b678173f1286972c93910f211d4cbb378
victor david santos <victor.santos1@penadinho.cec.ime.usp.br>
2014-05-09 15:43:36 -0300

    Ajustes gerais do layout.

--- physusp/WebContent/WEB-INF/jsp/energySystemsContributions/index.jsp
+++ physusp/WebContent/WEB-INF/jsp/energySystemsContributions/index.jsp
@@ -1,5 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
@@ -16,7 +15,7 @@
 		action="<c:url value="/esc/calculate"/>">
 		<div class="container">
 			<ul id="containerTabs" class="nav nav-tabs">
-				<li class="active"><a href="#options" data-toggle="tab">Options</a></li>
+				<li id="tabOptions" class="active"><a href="#options" data-toggle="tab">Options</a></li>
 				<li id="tab_aerobic"><a href="#aerobic" data-toggle="tab">Aerobic</a></li>
 				<li id="tab_anaerobicLactic"><a href="#anaerobicLactic" data-toggle="tab">Anaerobic
 						Lactic</a></li>
@@ -26,128 +25,87 @@
 			</ul>
 
 			<div id="containerPanes" class="tab-content">
-				<div id="options" class="tab-pane col-sm-12 active">
-					<p class="information">What do you wish to calculate?</p>
-					<div class="checkbox">
-						<label class="control-label"> <input type="checkbox"
-							name="parameters.calculateAerobic" value="true" 
-							data-tab="aerobic" checked /> Aerobic System
-						</label>
-					</div>
-					<div class="checkbox">
-						<label class="control-label"> <input type="checkbox"
-							name="parameters.calculateAnaerobicLactic" value="true"
-							data-tab="anaerobicLactic" checked /> Anaerobic Lactic System
-						</label>
-					</div>
-					<div class="checkbox">
-						<label class="control-label"> <input type="checkbox"
-							name="parameters.calculateAnaerobicAlactic" value="true"
-							data-tab="anaerobicAlactic" checked /> Anaerobic Alactic System
-						</label>
-					</div>
-					<div id="validateSystems">
-						<label class="control-label"></label>
+				<div id="options" class="tab-pane active">
+					<div class="col-sm-12">
+						<p class="information">What do you wish to calculate?</p>
+						<div class="checkbox">
+							<label class="control-label"> <input type="checkbox"
+								name="parameters.calculateAerobic" value="true" 
+								data-tab="aerobic" checked /> Aerobic System
+							</label>
+						</div>
+						<div class="checkbox">
+							<label class="control-label"> <input type="checkbox"
+								name="parameters.calculateAnaerobicLactic" value="true"
+								data-tab="anaerobicLactic" checked /> Anaerobic Lactic System
+							</label>
+						</div>
+						<div class="checkbox">
+							<label class="control-label"> <input type="checkbox"
+								name="parameters.calculateAnaerobicAlactic" value="true"
+								data-tab="anaerobicAlactic" checked /> Anaerobic Alactic System
+							</label>
+						</div>
+						<div id="validateSystems">
+							<label class="control-label"></label>
+						</div>
 					</div>
 					<button id="btnContinue" type="button" class="btn btn-lg btn-primary">Continue</button>
 				</div>
 
 				<div id="aerobic" class="tab-pane">
-					<div class="row">
-						<div class="col-md-6">
-							<label>How would you like to determine the rest O<sub>2</sub> consumption?</label>
-							<div class="radio">
-								<label class="control-label">
-									<input type="radio" name="restOxygenParameters.calculateMethod" value="ignore" checked />
-									I want to ignore the rest O<sub>2</sub> consumption
-								</label>
-							</div>
-							<div class="radio">
-								<label class="control-label">
-									<input type="radio" name="restOxygenParameters.calculateMethod" value="series" checked />
-									I have a table with values
-								</label>
-							</div>
-							<div class="radio">
-								<label class="control-label">
-									<input type="radio" name="restOxygenParameters.calculateMethod" value="fixed" />
-									I want to use a fixed value
-								</label>
-							</div>
-							
-							<div class="form-group col-md-12" id='aerobicRestTable'>
+					<div id="restData" class="col-sm-6">
+						<label>How would you like to determine the rest O<sub>2</sub> consumption?</label>
+						<div class="radio">
+							<label class="control-label">
+								<input type="radio" name="restOxygenParameters.calculateMethod" value="ignore" checked />
+								I want to ignore the rest O<sub>2</sub> consumption
+							</label>
+						</div>
+						<div class="radio">
+							<label class="control-label">
+								<input type="radio" name="restOxygenParameters.calculateMethod" value="series" checked />
+								I have a table with values
+							</label>
+						</div>
+						<div class="radio">
+							<label class="control-label">
+								<input type="radio" name="restOxygenParameters.calculateMethod" value="fixed" />
+								I want to use a fixed value
+							</label>
+						</div>
+						
+						<div class="col-sm-12">
+							<div class="form-group" id='aerobicRestTable'>
 								<div id="oxygenConsumptionRest"></div>
-								<!-- opções adicionais aqui -->
 							</div>
-							<div class="form-group col-md-12" style="display:none" id='aerobicRestAvg'>
+							<div class="form-group" id="aerobicRestAvg">
 								<label class="control-label">Rest O<sub>2</sub> consumption <strong>(ml/min)</strong></label>
 								<input type="text" name="restOxygenParameters.fixedValue" class="form-control" />
 							</div>
 						</div>
-						<div class="col-md-6">
-							<div class="form-group col-md-12 ">
-								<label class="control-label">O<sub>2</sub> consumption during exercise</label>
-								<div id="oxygenConsumptionDuringExercise"></div>
-							</div>
-						</div>
+					</div>
+					<div class="col-sm-6">
+						<label>O<sub>2</sub> consumption during exercise</label>
+						<div id="oxygenConsumptionDuringExercise"></div>
 					</div>
 				</div>
 
-
 				<div id="anaerobicLactic" class="tab-pane col-sm-12">
-					<div class="col-sm-3"></div>
 					<div class="col-sm-6">
 						<div class="form-group">
-							<label class="col-md-4 control-label">Body Mass <strong>(kg)</strong></label>
-							<div class="col-md-8">
-								<input type="text" name="anaerobicLacticParameters.weight" class="form-control" />
-							</div>
+							<label class="control-label">Body Mass <strong>(kg)</strong></label>
+							<input type="text" name="anaerobicLacticParameters.weight" class="form-control" />
 						</div>
 						<div class="form-group">
-							<label class="col-md-4 control-label">Rest <strong>(mmol/L)</strong></label>
-							<div class="col-md-8">
-								<input type="text" name="anaerobicLacticParameters.restLactateConcentration" class="form-control" />
-							</div>
+							<label class="control-label">Rest <strong>(mmol/L)</strong></label>
+							<input type="text" name="anaerobicLacticParameters.restLactateConcentration" class="form-control" />
 						</div>
 						<div class="form-group">
-							<label class="col-md-4 control-label">Peak <strong>(mmol/L)</strong></label>
-							<div class="col-md-8">
-								<input type="text" name="anaerobicLacticParameters.maxLactateConcentration" class="form-control" />
-							</div>
+							<label class="control-label">Peak <strong>(mmol/L)</strong></label>
+							<input type="text" name="anaerobicLacticParameters.maxLactateConcentration" class="form-control" />
 						</div>
 					</div>
-					<div class="col-sm-3"></div>
-				</div>
-
-
-				<div id="anaerobicLactic" class="tab-pane col-sm-12">
-					<div class="col-sm-3"></div>
-					<div class="col-sm-6">
-						<div class="form-group">
-							<label class="col-md-4 control-label">Body Mass <strong>(kg)</strong></label>
-							<div class="col-md-8">
-								<input type="text" name="anaerobicLacticParameters.weight"
-									class="form-control" />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-md-4 control-label">Rest <strong>(mmol/L)</strong></label>
-							<div class="col-md-8">
-								<input type="text"
-									name="anaerobicLacticParameters.restLactateConcentration"
-									class="form-control" />
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-md-4 control-label">Peak <strong>(mmol/L)</strong></label>
-							<div class="col-md-8">
-								<input type="text"
-									name="anaerobicLacticParameters.maxLactateConcentration"
-									class="form-control" />
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-3"></div>
 				</div>
 
 				<div id="anaerobicAlactic" class="tab-pane">
@@ -193,14 +151,16 @@
 					</div>
 				</div>
 
-
-				<div id="results" class="tab-pane col-sm-12">
-					<button id="exp-jpg" type="button" class="btn btn-default btn-lg"
-						onclick="exportChart('image/jpeg')">Export to JPG</button>
-					<button id="exp-png" type="button" class="btn btn-default btn-lg"
-						onclick="exportChart('image/png')">Export to PNG</button>
-					<button id="exp-svg" type="button" class="btn btn-default btn-lg"
-						onclick="exportChart('image/svg+xml')">Export to SVG</button>
+				<div id="results" class="tab-pane">
+					<div id="containerChart"></div>
+					<div class="text-center">
+						<button id="exp-jpg" type="button" class="exportButton btn btn-default btn-lg"
+							data-format="image/jpeg">Export to JPG</button>
+						<button id="exp-png" type="button" class="exportButton btn btn-default btn-lg"
+							data-format="image/png">Export to PNG</button>
+						<button id="exp-svg" type="button" class="exportButton btn btn-default btn-lg"
+							data-format="image/svg+xml">Export to SVG</button>
+					</div>
 				</div>
 			</div>
 
--- physusp/WebContent/js/energysystemscontributions.js
+++ physusp/WebContent/js/energysystemscontributions.js
@@ -1,177 +1,200 @@
-function setEnergySystems() {
-	var selectedCalculations = 0;
-	$("#options").find("input").each(function(){
-		var input = $(this);
-		if (input.is(":checked")) {
-			$("#tab_" + input.data("tab")).addClass("show");
-			selectedCalculations++;
-		}
-		else
-			$("#tab_" + input.data("tab")).removeClass("show");
-	});
+(function() {
+	var _chartOptions = null;
 	
-	if (selectedCalculations == 0) {
-		$("#validateSystems").addClass("has-error");
-		$("#validateSystems").find("label").text("Please select at least one energy system.");
-		return;
+	function setEnergySystems() {
+		var selectedCalculations = 0;
+		$("#options").find("input").each(function(){
+			var input = $(this);
+			if (input.is(":checked")) {
+				$("#tab_" + input.data("tab")).addClass("show");
+				selectedCalculations++;
+			}
+			else
+				$("#tab_" + input.data("tab")).removeClass("show");
+		});
+		
+		if (selectedCalculations == 0) {
+			$("#validateSystems").addClass("has-error");
+			$("#validateSystems").find("label").text("Please select at least one energy system.");
+			return;
+		}
+		
+		if ($("#tab_aerobic").hasClass("show")) {
+			var restData = $("#restData").detach();
+			$("#aerobic").prepend(restData);
+		}
+		else if ($("#tab_anaerobicAlactic").hasClass("show")) {
+			var restData = $("#restData").detach();
+			$("#anaerobicAlactic").prepend(restData);
+		}
+		
+		$("#containerTabs").find("li.show").first().find("a").tab("show");
+		$("#btnCalculate").show();
+		
+		$("#validateSystems").removeClass("has-error");
+		$("#validateSystems").find("label").text("");
 	}
 	
-	$("#containerTabs").find("li.show").first().find("a").tab("show");
-	$("#btnCalculate").show();
+	function prepareTableToSend(tableRows){
+		var toSend = "";
+		for(var i = 0; i < tableRows.length - 1; i++)
+			if(tableRows[i][0] != null) {
+				toSend += tableRows[i][0] + "," + tableRows[i][1] + "\n";
+			}
+		return toSend;
+	}
 	
-	$("#validateSystems").removeClass("has-error");
-	$("#validateSystems").find("label").text("");
-}
-
-function prepareTableToSend(tableRows){
-	var toSend = "";
-	for(var i = 0; i < tableRows.length - 1; i++)
-		if(tableRows[i][0] != null) {
-			toSend += tableRows[i][0] + "," + tableRows[i][1] + "\n";
-		}
-	return toSend;
-}
-
-function sendFormData(url, formData) {
-	$.ajax({
-		url: url,
-		type: "POST",
-		data: formData
-	})
-	.done(function(result){
-		var data = []; 
-		$.each(result.consumption, function(key, value) { data.push([key, value]);});
-		showChart(data);
-	});
-}
-
-function showChart(data) {
+	function sendFormData(url, formData) {
+		$.ajax({
+			url: url,
+			type: "POST",
+			data: formData
+		})
+		.done(function(result){
+			var data = []; 
+			$.each(result.consumption, function(key, value) { data.push([key, value]);});
+			showChart(data);
+		});
+	}
 	
-	$("#results").highcharts({
-		chart: {
-			plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-	    },
-	    title: {
-	        text: 'Energy system contributions'
-	    },
-	    tooltip: {
-	    	pointFormat: '{series.name}: <b>{point.y:.1f} kcal</b> <br/><b>{point.percentage:.1f}%</b>'
-	    },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    color: '#000000',
-                    connectorColor: '#000000',
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} % ({point.y:.1f} kcal)'
-                }
-            }
-        },
-        exporting: {
-        	buttons: {
-        		contextButton: {
-        			enabled: false
-        		}
-        	}
-        },
-        series: [{
-            type: 'pie',
-            name: 'Contribution',
-            data: data
-        }]
-	});
-	$('#exp-svg').show();
-	$('#exp-png').show();
-	$('#exp-jpg').show();
-	$("#tabResults").show();
-	$('#tabResults a').tab('show');
-	$("#btnCalculate").hide();
-}
-
-function exportChart(fileType) {
-	var chart = $('#results').highcharts();
-	chart.exportChart({
-		type: fileType,
-		filename: 'energy systems contributions ' + new Date()
-	});
-}
-
-$(function(){
+	function showChart(data) {
+		
+		_chartOptions = {
+			chart: {
+				plotBackgroundColor: null,
+	            plotBorderWidth: null,
+	            plotShadow: false
+		    },
+		    title: {
+		        text: 'Energy system contributions'
+		    },
+		    tooltip: {
+		    	pointFormat: '{series.name}: <b>{point.y:.1f} kcal</b> <br/><b>{point.percentage:.1f}%</b>'
+		    },
+	        plotOptions: {
+	            pie: {
+	                allowPointSelect: true,
+	                cursor: 'pointer',
+	                dataLabels: {
+	                    enabled: true,
+	                    color: '#000000',
+	                    connectorColor: '#000000',
+	                    format: '<b>{point.name}</b>: {point.percentage:.1f} % ({point.y:.1f} kcal)'
+	                }
+	            }
+	        },
+	        exporting: {
+	        	buttons: {
+	        		contextButton: {
+	        			enabled: false
+	        		}
+	        	}
+	        },
+	        series: [{
+	            type: 'pie',
+	            name: 'Contribution',
+	            data: data
+	        }]
+		};
+		$('#exp-svg').show();
+		$('#exp-png').show();
+		$('#exp-jpg').show();
+		$("#tabResults").show();
+		$('#tabResults a').tab('show');
+		$("#btnCalculate").hide();
+	}
 	
-	$("#btnContinue").click(setEnergySystems);
-	
-	$("#containerTabs a").click(function (e) {
-		e.preventDefault();
-		var link = $(this), tabId = link.parent().attr("id");
-		if (tabId == "tabOptions" || tabId == "tabResults")
-			$("#btnCalculate").hide();
-		else
-			$("#btnCalculate").show();
-		link.tab("show");
-	});
-	
-	$('#containerTabs a').on('shown.bs.tab', function (e) {
-		var tabName = $(this).attr('href');
-		$(tabName).find('.handsontable').each(function(index, element) {
-			for(var i = 0; i< 2; ++i) {
-				$(element).handsontable('render');
+	$(function(){
+		
+		$("#btnContinue").click(setEnergySystems);
+		
+		$("#containerTabs a").click(function (e) {
+			e.preventDefault();
+			var link = $(this), tabId = link.parent().attr("id");
+			if (tabId == "tabOptions" || tabId == "tabResults")
+				$("#btnCalculate").hide();
+			else
+				$("#btnCalculate").show();
+			link.tab("show");
+		});
+		
+		$('#containerTabs a').on('shown.bs.tab', function (e) {
+			var $tab = $($(this).attr('href'));
+			$tab.find('.handsontable').each(function(index, element) {
+				for(var i = 0; i< 2; ++i) {
+					$(element).handsontable('render');
+				}
+			});
+			if ($tab.attr("id") == "results") {
+				$("#containerChart").highcharts(_chartOptions);
 			}
 		});
-	});
-	
-	$("#data").submit(function(){
-		var input = $(this).serialize();
-		var url = $(this).attr("action");
 		
-		var oxygenConsumptionRest = $('#oxygenConsumptionRest').handsontable('getData');
-		input += "&restOxygenParameters.oxygenConsumptionRest=" + prepareTableToSend(oxygenConsumptionRest);
+		$("#data").submit(function(){
+			var input = $(this).serialize();
+			var url = $(this).attr("action");
+			
+			var oxygenConsumptionRest = $('#oxygenConsumptionRest').handsontable('getData');
+			input += "&restOxygenParameters.oxygenConsumptionRest=" + prepareTableToSend(oxygenConsumptionRest);
+			
+			var oxygenConsumptionDuringExercise = $('#oxygenConsumptionDuringExercise').handsontable('getData');
+			input += "&aerobicParameters.oxygenConsumptionDuringExercise=" + prepareTableToSend(oxygenConsumptionDuringExercise);
+			
+			var oxygenConsumptionPostExercise = $('#oxygenConsumptionPostExercise').handsontable('getData');
+			input += "&anaerobicAlacticParameters.oxygenConsumptionPostExercise=" + prepareTableToSend(oxygenConsumptionPostExercise);
+			
+			sendFormData(url, input);
+			
+			return false;
+		});
 		
-		var oxygenConsumptionDuringExercise = $('#oxygenConsumptionDuringExercise').handsontable('getData');
-		input += "&aerobicParameters.oxygenConsumptionDuringExercise=" + prepareTableToSend(oxygenConsumptionDuringExercise);
+		var headers = ["Time <strong>(hh:mm:ss)</strong>", "VO<sub>2</sub> <strong>(ml/min)</strong>"];
 		
-		var oxygenConsumptionPostExercise = $('#oxygenConsumptionPostExercise').handsontable('getData');
-		input += "&anaerobicAlacticParameters.oxygenConsumptionPostExercise=" + prepareTableToSend(oxygenConsumptionPostExercise);
+		$('#oxygenConsumptionRest').handsontable({
+		    minSpareRows: 10,
+		    colHeaders: headers,
+		    contextMenu: true,
+		    data: [[null, null]],
+		    height: 260,
+		    colWidths: [1, 1],
+		    stretchH: "all"
+		});
+		$('#oxygenConsumptionDuringExercise').handsontable({
+		    minSpareRows: 10,
+		    colHeaders: headers,
+		    contextMenu: true,
+		    data: [[null, null]],
+		    height: 260,
+		    colWidths: [1, 1],
+		    stretchH: "all"
+		});
+		$('#oxygenConsumptionPostExercise').handsontable({
+		    minSpareRows: 10,
+		    colHeaders: headers,
+		    contextMenu: true,
+		    data: [[null, null]],
+		    height: 260,
+		    colWidths: [1, 1],
+		    stretchH: "all"
+		});
 		
-		sendFormData(url, input);
+		$('#aerobic input:radio').change(function() {
+			$("#aerobicRestTable").toggle($(this).val() == "series");
+			$("#aerobicRestAvg").toggle($(this).val() == "fixed");
+		});
 		
-		return false;
+		$('#useTimeDelay').change(function() {
+			$("#TimeDelayDiv").toggle();
+		});
+		
+		$(".exportButton").click(function() {
+			var format = $(this).data("format");
+			var chart = $("#containerChart").highcharts();
+			chart.exportChart({
+				type: format,
+				filename: 'energy systems contributions ' + new Date()
+			});
+		});
+		
 	});
-	
-	var headers = ["Time <strong>(hh:mm:ss)</strong>", "VO<sub>2</sub> <strong>(ml/min)</strong>"];
-	
-	$('#oxygenConsumptionRest').handsontable({
-	    minSpareRows: 10,
-	    colHeaders: headers,
-	    contextMenu: true,
-	    data: [[null, null]],
-	    height: 100
-	});
-	$('#oxygenConsumptionDuringExercise').handsontable({
-	    minSpareRows: 10,
-	    colHeaders: headers,
-	    contextMenu: true,
-	    data: [[null, null]],
-	    height: 100
-	});
-	$('#oxygenConsumptionPostExercise').handsontable({
-	    minSpareRows: 10,
-	    colHeaders: headers,
-	    contextMenu: true,
-	    data: [[null, null]],
-	    height: 100
-	});
-	
-	$('#aerobic input:radio').change(function() {
-		$("#aerobicRestTable").toggle($(this).val() == "series");
-		$("#aerobicRestAvg").toggle($(this).val() == "fixed");
-	});
-	
-	$('#useTimeDelay').change(function() {
-		$("#TimeDelayDiv").toggle();
-	});
-	
-});
+})();
