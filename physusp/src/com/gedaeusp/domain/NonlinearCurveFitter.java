91662f35cbdf30023db7e3db8341044a332d6a9f
thales areco bandiera paiva <thales.paiva@penadinho.cec.ime.usp.br>
2014-06-18 15:22:07 -0300

    Sempre otimiza para VO2 (baseline) no cálculo do Anaeróbio Alático

--- physusp/src/com/gedaeusp/domain/NonlinearCurveFitter.java
+++ physusp/src/com/gedaeusp/domain/NonlinearCurveFitter.java
@@ -2,6 +2,7 @@ package com.gedaeusp.domain;
 
 import org.apache.commons.math3.fitting.CurveFitter;
 import org.apache.commons.math3.optim.nonlinear.vector.jacobian.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.util.FastMath;
 
 import br.com.caelum.vraptor.ioc.Component;
@@ -27,9 +28,9 @@ public class NonlinearCurveFitter {
 			double v0, double t0) {
 
 		double[] exp = doExponentialFit(v, t, v0);
-
 		double initT0 = (t0 < 0) ? -1 : t0;
-		double initV0 = exp[Exponential.PARAM_v0];
+		double initV0 = StatUtils.min(v);
+		System.out.println("600 ? initV0 = " + initV0);
 		double initTau = exp[Exponential.PARAM_tau];
 		double initA = exp[Exponential.PARAM_a] * 0.5
 				* FastMath.exp(-initT0 / initTau);
@@ -44,7 +45,8 @@ public class NonlinearCurveFitter {
 
 		double[] exp = doExponentialFit(v, t, v0);
 		double initT0 = (t0 < 0) ? -1 : t0;
-		double initV0 = exp[Exponential.PARAM_v0];
+		double initV0 = StatUtils.min(v);
+		System.out.println("600 ? initV0 = " + initV0);
 		double initTau = exp[Exponential.PARAM_tau];
 		double initA = exp[Exponential.PARAM_a] * FastMath.exp(-initT0 / initTau);
 
@@ -53,41 +55,34 @@ public class NonlinearCurveFitter {
 
 
 	public double[] doExponentialFit(double[] v, double[] t, double v0) {
-		boolean fixedV0 = true;
-
+		boolean fixedV0 = false;
 		CurveFitter<ExponentialParametric> fitter = new CurveFitter<ExponentialParametric>(
 				new LevenbergMarquardtOptimizer());
-
 		addObservedPointsToFitter(v, t, fitter);
-
 
 		return fitter.fit(new ExponentialParametric(fixedV0), new double[] { v0, 1, 1 });
 	}
 
 
-	public double[] doDelayedExponentialFit(double[] v, double[] t,
-			double[] init) {
-		boolean fixedV0 = true;
+	public double[] doDelayedExponentialFit(double[] v, double[] t, double[] init) {
+		boolean fixedV0 = false;
 		boolean fixedT0 = (init[Biexponential.PARAM_t0] >= 0);
 		CurveFitter<DelayedExponentialParametric> fitter = new CurveFitter<DelayedExponentialParametric>(
 				new LevenbergMarquardtOptimizer());
-
 		addObservedPointsToFitter(v, t, fitter);
-
 
 		return fitter.fit(new DelayedExponentialParametric(fixedV0, fixedT0), init);
 	}
 
 
 	public double[] doBiexponentialFit(double[] v, double[] t, double[] init) {
-		
-		boolean fixedV0 = true;
+		boolean fixedV0 = false;
 		boolean fixedT0 = (init[Biexponential.PARAM_t0] >= 0);
-		
 		CurveFitter<BiexponentialParametric> fitter = new CurveFitter<BiexponentialParametric>(
 				new LevenbergMarquardtOptimizer());
-
 		addObservedPointsToFitter(v, t, fitter);
+		
 		return fitter.fit(new BiexponentialParametric(fixedV0, fixedT0), init);
 	}
+	
 }
